23545173588d65f1f8d00001386ca2ef6bb45ab4
Chris Allen <58678379+Skelt3r@users.noreply.github.com>
2021-02-22 15:31:29 -0600

    Convert message functions to dictionaries

--- data/messages.py
+++ data/messages.py
@@ -1,36 +1,51 @@
+  
 # Default welcome message
-def welcome():
-    return {'message': 'Welcome to the 5e RNG API! See the available functions below.',
-            'functions': {
-                'GET': {
-                    '/roll/XdX': {'description': 'Use the following syntax for rolling dice.',
-                                  'examples': ['/roll/1d20',
-                                               '/roll/2d6+5',
-                                               '/roll/4d4-1']},
-                    '/roll/stats': {'description': 'Randomly generate a set of 5e ability scores.'}}}}
-
+welcome = {
+    'message': 'Welcome to the 5e RNG API! See the available functions below.',
+    'functions': {
+        'GET': {
+            '/roll/XdX': {
+                'description': 'Use the following syntax for rolling dice.',
+                'examples': [
+                    '/roll/1d20',
+                    '/roll/2d6+5',
+                    '/roll/4d4-1'
+                ]
+            },
+            '/roll/stats': {
+                'description': 'Randomly generate a set of 5e ability scores.'
+            },
+            '/generate/pc': {
+                'description': 'Randomly generate a 5e DND character.'
+            }
+        }
+    }
+}
 
 # Error message for invalid input
-def invalid_input():
-    return {'error': 'invalid-input',
-            'message': 'Bad request! Please enter a valid endpoint.',
-            'examples': ['/roll/1d20',
-                         '/roll/2d6',
-                         '/roll/4d4+5',
-                         '/roll/stats',
-                         '/generate/pc'],
-            'status': 400}
-
+invalid_input = {
+    'error': 'invalid-input',
+    'message': 'Bad request! Please enter a valid endpoint.',
+    'examples': [
+        '/roll/1d20',
+        '/roll/2d6',
+        '/roll/4d4+5',
+        '/roll/stats',
+        '/generate/pc'
+    ],
+    'status': 400
+}
 
 # Error message for an invalid roll count
-def invalid_roll_count():
-    return {'error': 'invalid-roll-count',
-            'message': 'The endpoint accepts a minimum of 1 roll or a maximum of 100 rolls.',
-            'status': 400}
-
+invalid_roll_count = {
+    'error': 'invalid-roll-count',
+    'message': 'The endpoint accepts a minimum of 1 roll or a maximum of 100 rolls.',
+    'status': 400
+}
 
 # Error message for an invalid dice type
-def invalid_dice_type():
-    return {'error': 'invalid-dice-type',
-            'message': 'The endpoint accepts a range of d2-d100 dice types.',
-            'status': 400}
+invalid_dice_type = {
+    'error': 'invalid-dice-type',
+    'message': 'The endpoint accepts a range of d2-d100 dice types.',
+    'status': 400
+}
