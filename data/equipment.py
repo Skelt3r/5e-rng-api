5138ce4fb5bc725eaea4e44e4d9ae124854fcd33
Chris Allen <58678379+Skelt3r@users.noreply.github.com>
2021-02-24 17:44:22 -0600

    Store equipment data in a class

--- data/equipment.py
+++ data/equipment.py
@@ -1,199 +1,201 @@
-# Dict of armor
-armor = {
-    'light': [
-        'padded',
-        'leather',
-        'studded leather'
-    ],
-    'medium': [
-        'hide',
-        'chain shirt',
-        'scale mail',
-        'breastplate',
-        'half plate'
-    ],
-    'heavy': [
-        'ring mail',
-        'chain mail',
-        'splint',
-        'plate'
-    ]
-}
-
-# Dict of weapon names grouped by type/class
-weapons = {
-    'melee': {
-        'simple': [
-            'club',
-            'dagger',
-            'greatclub',
-            'handaxe',
-            'javelin',
-            'light hammer',
-            'mace',
-            'quarterstaff',
-            'sickle',
-            'spear'
+# Class for storing and retrieving equipment data
+class EquipmentData():
+    # Dict of armor
+    armor = {
+        'light': [
+            'padded',
+            'leather',
+            'studded leather'
         ],
-        'martial': [
-            'battleaxe',
-            'flail',
-            'glaive',
-            'greataxe',
-            'greatsword',
-            'halberd',
-            'lance',
-            'longsword',
-            'maul',
-            'morningstar',
-            'pike',
-            'rapier',
-            'scimitar',
-            'shortsword',
-            'trident',
-            'war pick',
-            'warhammer',
-            'whip'
-        ]
-    },
-    'ranged': {
-        'simple': [
-            'light crossbow',
-            'dart',
-            'shortbow',
-            'sling'
+        'medium': [
+            'hide',
+            'chain shirt',
+            'scale mail',
+            'breastplate',
+            'half plate'
         ],
-        'martial': [
-            'blowgun',
-            'hand crossbow',
-            'heavy crossbow',
-            'longbow',
-            'net'
+        'heavy': [
+            'ring mail',
+            'chain mail',
+            'splint',
+            'plate'
         ]
     }
-}
 
-# List of tool sets
-tools = [
-    "alchemist's supplies",
-    "brewer's supplies",
-    "calligrapher's supplies",
-    "carpenter's tools",
-    "cartographer's tools",
-    "cobbler's tools",
-    "cook's utensils",
-    "glassblower's tools",
-    "jeweler's tools",
-    "leatherworker's tools",
-    "mason's tools",
-    "painter's supplies",
-    "potter's tools",
-    "smith's tools",
-    "tinker's tools",
-    "weaver's tools",
-    "woodcarver's tools"
-]
+    # Dict of weapon names grouped by type/class
+    weapons = {
+        'melee': {
+            'simple': [
+                'club',
+                'dagger',
+                'greatclub',
+                'handaxe',
+                'javelin',
+                'light hammer',
+                'mace',
+                'quarterstaff',
+                'sickle',
+                'spear'
+            ],
+            'martial': [
+                'battleaxe',
+                'flail',
+                'glaive',
+                'greataxe',
+                'greatsword',
+                'halberd',
+                'lance',
+                'longsword',
+                'maul',
+                'morningstar',
+                'pike',
+                'rapier',
+                'scimitar',
+                'shortsword',
+                'trident',
+                'war pick',
+                'warhammer',
+                'whip'
+            ]
+        },
+        'ranged': {
+            'simple': [
+                'light crossbow',
+                'dart',
+                'shortbow',
+                'sling'
+            ],
+            'martial': [
+                'blowgun',
+                'hand crossbow',
+                'heavy crossbow',
+                'longbow',
+                'net'
+            ]
+        }
+    }
 
-# List of gaming sets
-gaming_sets = [
-    'dice set',
-    'playing card set'
-]
+    # List of tool sets
+    tools = [
+        "alchemist's supplies",
+        "brewer's supplies",
+        "calligrapher's supplies",
+        "carpenter's tools",
+        "cartographer's tools",
+        "cobbler's tools",
+        "cook's utensils",
+        "glassblower's tools",
+        "jeweler's tools",
+        "leatherworker's tools",
+        "mason's tools",
+        "painter's supplies",
+        "potter's tools",
+        "smith's tools",
+        "tinker's tools",
+        "weaver's tools",
+        "woodcarver's tools"
+    ]
 
-# List of musical instruments
-instruments = [
-    'bagpipes',
-    'drum',
-    'dulcimer',
-    'flute',
-    'lute',
-    'lyre',
-    'horn',
-    'pan flute',
-    'shawm',
-    'viol'
-]
+    # List of gaming sets
+    gaming_sets = [
+        'dice set',
+        'playing card set'
+    ]
 
-# Dict of starting equipment
-equipment_packs = {
-    'burglar': [
-        'backpack',
-        '1,000 ball bearings',
-        '10 feet of string',
-        'bell', 
-        '5 candles',
-        'crowbar',
-        'hammer',
-        '10 pitons',
-        'hooded lantern',
-        '2 flasks of oil',
-        '5 days of rations',
-        'tinderbox',
-        'waterskin',
-        '50 feet of hempen rope'
-    ],
-    'diplomat': [
-        'chest',
-        'case for maps',
-        'case for scrolls',
-        'set of fine clothes',
-        'bottle of ink',
-        'ink pen',
-        'lamp',
-        '2 flasks of oil',
-        '5 sheets of paper',
-        'vial of perfume',
-        'sealing wax',
-        'soap'
-    ],
-    'dungeoneer': [
-        'backpack',
-        'crowbar',
-        'hammer',
-        '10 pitons',
-        '10 torches',
-        'tinderbox',
-        '10 days of rations',
-        'waterskin'
-    ],
-    'entertainer': [
-        'backpack',
-        'bedroll',
-        '2 costumes',
-        '5 candles',
-        '5 days of rations',
-        'waterskin',
-        'disguise kit'
-    ],
-    'explorer': [
-        'backpack',
-        'bedroll',
-        'mess kit',
-        'tinderbox',
-        '10 torches',
-        '10 days of rations',
-        'waterskin',
-        '50 feet of hempen rope'
-    ],
-    'priest': [
-        'backpack',
-        'blanket',
-        '10 candles',
-        'tinderbox',
-        'alm box',
-        '2 blocks of incense',
-        'censer',
-        'vestments',
-        '2 days of rations',
-        'waterskin'
-    ],
-    'scholar': [
-        'backpack',
-        'book of lore',
-        'bottle of ink',
-        'ink pen',
-        '10 sheets of parchment',
-        'small bag of sand',
-        'small knife'
-    ],
-}
+    # List of musical instruments
+    instruments = [
+        'bagpipes',
+        'drum',
+        'dulcimer',
+        'flute',
+        'lute',
+        'lyre',
+        'horn',
+        'pan flute',
+        'shawm',
+        'viol'
+    ]
+
+    # Dict of starting equipment
+    equipment_packs = {
+        'burglar': [
+            'backpack',
+            '1,000 ball bearings',
+            '10 feet of string',
+            'bell', 
+            '5 candles',
+            'crowbar',
+            'hammer',
+            '10 pitons',
+            'hooded lantern',
+            '2 flasks of oil',
+            '5 days of rations',
+            'tinderbox',
+            'waterskin',
+            '50 feet of hempen rope'
+        ],
+        'diplomat': [
+            'chest',
+            'case for maps',
+            'case for scrolls',
+            'set of fine clothes',
+            'bottle of ink',
+            'ink pen',
+            'lamp',
+            '2 flasks of oil',
+            '5 sheets of paper',
+            'vial of perfume',
+            'sealing wax',
+            'soap'
+        ],
+        'dungeoneer': [
+            'backpack',
+            'crowbar',
+            'hammer',
+            '10 pitons',
+            '10 torches',
+            'tinderbox',
+            '10 days of rations',
+            'waterskin'
+        ],
+        'entertainer': [
+            'backpack',
+            'bedroll',
+            '2 costumes',
+            '5 candles',
+            '5 days of rations',
+            'waterskin',
+            'disguise kit'
+        ],
+        'explorer': [
+            'backpack',
+            'bedroll',
+            'mess kit',
+            'tinderbox',
+            '10 torches',
+            '10 days of rations',
+            'waterskin',
+            '50 feet of hempen rope'
+        ],
+        'priest': [
+            'backpack',
+            'blanket',
+            '10 candles',
+            'tinderbox',
+            'alm box',
+            '2 blocks of incense',
+            'censer',
+            'vestments',
+            '2 days of rations',
+            'waterskin'
+        ],
+        'scholar': [
+            'backpack',
+            'book of lore',
+            'bottle of ink',
+            'ink pen',
+            '10 sheets of parchment',
+            'small bag of sand',
+            'small knife'
+        ],
+    }
